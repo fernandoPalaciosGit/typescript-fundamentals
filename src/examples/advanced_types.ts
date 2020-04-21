e1971718492222ee7c4ce7ed537e6d005cecb228
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-04-21 16:57:29 +0200

    advanced types: keyof (diccionario de tipos), infer (ternarios en generics)

---
+++ src/examples/advanced_types.ts
@@ -0,0 +1,34 @@
+// KEYOF
+export type PhoneContact = {
+    mobile: number;
+    home: number;
+};
+
+export type EmailContact = {
+    address: string;
+};
+
+export interface ContactTypes {
+    email: EmailContact;
+    phone: PhoneContact;
+    fax: { line: number }
+}
+
+const contact = <T extends keyof ContactTypes>(method: T, contact: ContactTypes[T]): void => {
+}
+
+contact('fax', { line: 654 });
+contact('email', { address: 'string' });
+contact("phone", { home: 654, mobile: 987 });
+contact("test", { home: 654, mobile: 987 }); // $ExpectError Argument of type '"test"' is not assignable to parameter of type '"email" | "phone" | "fax"'.
+contact("email", { home: 654, mobile: 987 }); // $ExpectError Argument of type '{ home: number; mobile: number; }' is not assignable to parameter of type 'EmailContact'.
+
+// TYPEOF :  se utiliza a modo de query de un typo
+export type TypePromise = typeof Promise.resolve; // ¿what return the type of Promise.resolve?
+export type TypePromiseSame = Promise<Response>;
+
+// INFER --> solo se puede usar en genericos para calcular con ternarios el tipo de valor
+export type EventualType<T> = T extends Promise<infer S> ? S : T
+
+let test: EventualType<number[]>;
+let testTest: EventualType<Promise<number>>;
